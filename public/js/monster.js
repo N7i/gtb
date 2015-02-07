88ff490b1f995c51d3adb897c3a713a2ad4ca697
damien <damdam.gold@gmail.com>
2015-02-07 03:29:38 +0100

    Add the monster manager

--- public/js/monster.js
+++ public/js/monster.js
@@ -1,5 +1,18 @@
 "use strict";
 
 var Monster = {
+    Init: function()
+            {
+                Monster.GenMonster();
+            },
 
+    GenMonster: function()
+                {
+                    Main.monster = BABYLON.Mesh.CreateBox("monster", 2, Main.scene);
+                    Main.monster.position.y = 1.0;
+                    Main.monster.position.x = -15.0;
+                    Main.monster.position.z = -10.0;
+                    Main.monster.material = new BABYLON.StandardMaterial("monster", Main.scene);
+                    Main.monster.material.diffuseTexture = new BABYLON.Texture("public/img/box0.png", Main.scene);
+                }
 };
