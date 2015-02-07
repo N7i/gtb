e9192057bada0e8a20328771c151bd92fe7257af
damien <damdam.gold@gmail.com>
2015-02-07 03:30:11 +0100

    Add the player manager

--- public/js/player.js
+++ public/js/player.js
@@ -1,5 +1,18 @@
 "use strict";
 
 var Player = {
+    Init: function()
+            {
+                Player.GenPlayer();
+            },
 
+    GenPlayer: function()
+                {
+                    Main.player = BABYLON.Mesh.CreateBox("player", 2, Main.scene);
+                    Main.player.position.y = 1.0;
+                    Main.player.position.x = 15.0;
+                    Main.player.position.z = 10.0;
+                    Main.player.material = new BABYLON.StandardMaterial("player", Main.scene);
+                    Main.player.material.diffuseTexture = new BABYLON.Texture("public/img/box0.png", Main.scene);
+                }
 };
